64c99acf8558b43c677c996aba0976b565b211e5
Oskar Holmberg <oho@netcompany.com>
2017-07-13 15:16:39 +0200

    Setup repository pattern base.

--- LFGo/App_Start/Startup.Auth.cs
+++ LFGo/App_Start/Startup.Auth.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using LFGo.Models;
+using LFGo.Database;
 
 namespace LFGo
 {
---
+++ LFGo/Database/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using LFGo.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace LFGo.Database
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
